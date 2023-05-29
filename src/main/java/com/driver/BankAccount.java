9a5dde3a83f631a23913a527835834c1198e250c
Neelam Dubey <ashidubey8208@gmail.com>
2023-05-29 11:49:43 +0530

    Second try

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -1,4 +1,9 @@
 package com.driver;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 public class BankAccount {
 
@@ -10,22 +15,46 @@ public class BankAccount {
 
     }
 
+    public BankAccount() {
+    }
+
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i< sb.length();i++)
+        {
+            if(sum>=9)
+            {
+                sb.append(9);
+                sum-=9;
+            }else {
+                sb.append(sum);
+                sum=0;
+            }
+            if(sum>0){
+                throw new Exception("Account Number can not be generated");
+            }return sb.toString();
+
+        }
 
         return null;
     }
 
     public void deposit(double amount) {
+       this.balance+=amount;
         //add amount to balance
 
     }
 
     public void withdraw(double amount) throws Exception {
-        // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        double remainBalance=this.balance-amount;
+        if (remainBalance<this.minBalance){
+            throw new Exception("Insufficient Balance");
+        }else {
+            this.balance-=amount;
+        }
     }
 
 }
